9dee05cad63c180083d6de94573839784bd68a1c
Manash Sahoo <manashkalyon@gmail.com>
2026-09-16 16:44:09 -0500

    Update plots.py

--- ui/plots.py
+++ ui/plots.py
@@ -37,7 +37,6 @@ class EcgPlot(pg.PlotWidget):
     def setup_plot_items(self):
         self.ecg_line = pg.PlotDataItem(symbol='o', pen='g', symbolBrush='g', symbolSize=2.5, width=1)
         self.filt_line = pg.PlotDataItem(symbol='o', pen='w', symbolBrush='w', symbolSize=2.5, width=1)
-        self.filt_line.setAlpha(.1,False)
         self.heartbeats_line = pg.PlotDataItem(pen=None, symbolPen='r', symbolBrush='r', symbol='t')
 
         self.point_selector = self.SelectedPoint(
